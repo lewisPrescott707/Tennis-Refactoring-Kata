6d74ce5db16e600e74b9fef9910ae399538777bb
prescottlewis707 <prescottlewis707@gmail.com>
2022-05-20 09:17:46 +0100

    lesson 1

--- typescript-jasmine/src/TennisGame1.ts
+++ typescript-jasmine/src/TennisGame1.ts
@@ -3,13 +3,6 @@ import { TennisGame } from './TennisGame';
 export class TennisGame1 implements TennisGame {
   private m_score1: number = 0;
   private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
-
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
 
   wonPoint(playerName: string): void {
     if (playerName === 'player1')
@@ -37,13 +30,6 @@ export class TennisGame1 implements TennisGame {
           break;
 
       }
-    }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
     }
     else {
       for (let i = 1; i < 3; i++) {
--- typescript-jasmine/tests/TennisTest.spec.ts
+++ typescript-jasmine/tests/TennisTest.spec.ts
@@ -1,43 +1,17 @@
-import scores from './scores';
-import { TennisGame, TennisGame1, TennisGame2, TennisGame3 } from '../src';
-
-function checkScore(game: TennisGame, player1Score: number, player2Score: number, expectedScore: string): void {
-  const highestScore: number = Math.max(player1Score, player2Score);
-  for (let i = 0; i < highestScore; i++) {
-    if (i < player1Score) {
-      game.wonPoint('player1');
-    }
-    if (i < player2Score) {
-      game.wonPoint('player2');
-    }
-  }
-  expect(game.getScore()).toEqual(expectedScore);
-}
+import { TennisGame1 } from '../src';
 
 describe('TennisGame', function () {
 
   describe('TennisGame1', function () {
-    scores.forEach(([player1Score, player2Score, expectedScore]) => {
-      it(`scores ${player1Score}:${player2Score} as ${expectedScore}`, function () {
-        checkScore(new TennisGame1('player1', 'player2'), player1Score, player2Score, expectedScore);
-      });
+    it(`scores 0:0 as Love-All`, function () {
+      const game = new TennisGame1();
+      expect(game.getScore()).toBe("Love-All");
+    });
+
+    it(`scores 15:0 as Fifteen-Love`, function () {
+      const game = new TennisGame1();
+      game.wonPoint('player2')
+      expect(game.getScore()).toBe("Fifteen-Love");
     });
   });
-
-  describe('TennisGame2', function () {
-    scores.forEach(([player1Score, player2Score, expectedScore]) => {
-      it(`scores ${player1Score}:${player2Score} as ${expectedScore}`, function () {
-        checkScore(new TennisGame2('player1', 'player2'), player1Score, player2Score, expectedScore);
-      });
-    });
-  });
-
-  describe('TennisGame3', function () {
-    scores.forEach(([player1Score, player2Score, expectedScore]) => {
-      it(`scores ${player1Score}:${player2Score} as ${expectedScore}`, function () {
-        checkScore(new TennisGame3('player1', 'player2'), player1Score, player2Score, expectedScore);
-      });
-    });
-  });
-
 });
